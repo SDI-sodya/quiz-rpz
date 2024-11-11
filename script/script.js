845733b10308313188d0c17c7eaf1d360886f524
SDI-sodya <saienko.dan06sdi@gmail.com>
2024-11-11 21:39:58 +0200

    Add script to change questions and answers

--- script/script.js
+++ script/script.js
@@ -4,6 +4,84 @@ document.addEventListener('DOMContentLoaded', function () {
   const closeModal = document.querySelector('#closeModal');
   const questionTitle = document.querySelector('#question');
   const formAnswers = document.querySelector('#formAnswers');
+  
+  const nextButton = document.querySelector('#next');
+  const prevButton = document.querySelector('#prev');
+
+  const questions = [
+    {
+      question: "Якого кольору бургер?",
+      answers: [
+        {
+          title: 'Стандарт',
+          url: './image/burger.png'
+        },
+        {
+          title: 'Чорний',
+          url: './image/burgerBlack.png'
+        }
+      ],
+      type: 'radio'
+    },
+    {
+      question: "З якого м'яса котлета?",
+      answers: [
+        {
+          title: 'Курка',
+          url: './image/chickenMeat.png'
+        },
+        {
+          title: 'Яловичина',
+          url: './image/beefMeat.png'
+        },
+        {
+          title: 'Свинина',
+          url: './image/porkMeat.png'
+        }
+      ],
+      type: 'radio'
+    },
+    {
+      question: "Додаткові інгредієнти ?",
+      answers: [
+        {
+          title: 'Помідор',
+          url: './image/tomato.png'
+        },
+        {
+          title: 'Огірок',
+          url: './image/cucumber.png'
+        },
+        {
+          title: 'Салат',
+          url: './image/salad.png'
+        },
+        {
+          title: 'Цибуля',
+          url: './image/onion.png'
+        }
+      ],
+      type: 'checkbox'
+    },
+    {
+    question: "Додати соус?",
+    answers: [
+      {
+        title: 'Часниковий',
+        url: './image/sauce1.png'
+      },
+      {
+        title: 'Томатний',
+        url: './image/sauce2.png'
+      },
+      {
+        title: 'Гірчичний',
+        url: './image/sauce3.png'
+      }
+    ],
+    type: 'radio'
+    }
+  ];
 
   btnOpenModal.addEventListener('click', () => {
     modalBlock.classList.add('d-block');
@@ -15,28 +93,40 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 
   const playTest = () => {
-    const renderQuestions = () => {
-      questionTitle.textContent = "Какого цвета бургер вы хотите?";
+    let numberQuestion = 0;
 
-      whiteBurger = `../image/burger.png`;
-      blackBurger = `../image/burgerBlack.png`;
+    const renderAnswers = (index) => {
+      questions[index].answers.forEach((answer) => {
+        const answerItem = document.createElement('div');
+  
+        answerItem.classList.add('answers-item', 'd-flex', 'flex-column');
+  
+        answerItem.innerHTML = `
+        <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+        <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+          <img class="answerImg" src="${answer.url}" alt="burger">
+          <span>${answer.title}</span>
+        </label>`;
+        formAnswers.appendChild(answerItem);
+      });
+    }
 
-      formAnswers.innerHTML = `
-      <div class="answers-item d-flex flex-column">
-        <input type="radio" id="answerItem1" name="answer" class="d-none">
-        <label for="answerItem1" class="d-flex flex-column justify-content-between">
-          <img class="answerImg" src="${whiteBurger}" alt="burger">
-          <span>Стандарт</span>
-        </label>
-      </div>
-      <div class="answers-item d-flex justify-content-center">
-        <input type="radio" id="answerItem2" name="answer" class="d-none">
-        <label for="answerItem2" class="d-flex flex-column justify-content-between">
-          <img class="answerImg" src="${blackBurger}" alt="burger">
-          <span>Чорний</span>
-        </label>
-      </div>`;
+    const renderQuestions = (indexQuestion) => {
+      formAnswers.innerHTML = '';
+      questionTitle.textContent = `${questions[indexQuestion].question}`;
+      
+      renderAnswers(indexQuestion);
     };
-    renderQuestions();
+    renderQuestions(numberQuestion);
+
+    nextButton.onclick = () => {
+      numberQuestion++;
+      renderQuestions(numberQuestion);
+    }
+
+    prevButton.onclick = () => {
+      numberQuestion--;
+      renderQuestions(numberQuestion);
+    }
   };
 });
